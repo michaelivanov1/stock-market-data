aee746003058ac36f237254205361dff7f362c80
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2022-04-07 12:07:03 -0400

    added canvasjs graph

--- src/Components/DataVisualizationComponent.js
+++ src/Components/DataVisualizationComponent.js
@@ -18,12 +18,15 @@ import moment from "moment";
 
 import theme from "../theme";
 
+import CanvasJSReact from '../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 
 const DataVisualizationComponent = () => {
 
     let yesterday = new Date(Date.now() - 86400000); // 24 * 60 * 60 * 1000
 
-    const [state, setState] = useState([
+    const [date, setDate] = useState([
         {
             startDate: new Date(yesterday),
             endDate: new Date(),
@@ -31,8 +34,42 @@ const DataVisualizationComponent = () => {
         }
     ]);
 
-    console.log(`startDate: ${(moment(state[0].startDate).format("l"))}`);
-    console.log(`endDate: ${(moment(state[0].endDate).format("l"))}`);
+    const options = {
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+            // text: "Intel Corporation Stock Price -  2017"
+        },
+        axisX: {
+            valueFormatString: "MMM"
+        },
+        axisY: {
+            includeZero: false,
+            prefix: "$",
+            title: "Price (in USD)"
+        },
+        data: [{
+            type: "candlestick",
+            showInLegend: true,
+            name: "Intel Corporation",
+            yValueFormatString: "$###0.00",
+            xValueFormatString: "MMMM YY",
+            dataPoints: [
+
+                { x: new Date(date[0].startDate), y: [44.73, 47.64, 42.67, 46.16] },
+                { x: new Date(date[0].endDate), y: [44.73, 47.64, 42.67, 46.16] },
+                // { x: new Date("2017-11-04"), y: [44.73, 47.64, 42.67, 46.16] },
+                // { x: new Date("2017-11-05"), y: [44.73, 47.64, 42.67, 46.16] },
+                // { x: new Date("2017-11-06"), y: [44.73, 47.64, 42.67, 46.16] },
+
+            ]
+        }
+        ]
+    }
+
+    // console.log(`startDate: ${(moment(date[0].startDate).format("l"))}`);
+    // console.log(`endDate: ${(moment(date[0].endDate).format("l"))}`);
 
     return (
         <ThemeProvider theme={theme}>
@@ -46,10 +83,20 @@ const DataVisualizationComponent = () => {
 
                 <DateRange
                     editableDateInputs={true}
-                    onChange={item => setState([item.selection])}
+                    onChange={item => setDate([item.selection])}
                     moveRangeOnFirstSelection={false}
-                    ranges={state}
+                    ranges={date}
                 />
+                <Card style={{maxWidth: '50%', marginLeft: '25%'}}>
+                    <div>
+                        {/* <h1>React Candlestick Chart</h1> */}
+                        <CanvasJSChart options={options}
+                        /* onRef={ref => this.chart = ref} */
+                        />
+                        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+                    </div>
+                </Card>
+
 
             </Box>
         </ThemeProvider>
