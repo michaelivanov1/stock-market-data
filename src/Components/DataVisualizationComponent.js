a1492faf2bb886134da532798b565bba8878aa46
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2022-04-27 16:00:59 -0400

    replaced cavasjs with plotly & added since inception graph

--- src/Components/DataVisualizationComponent.js
+++ src/Components/DataVisualizationComponent.js
@@ -1,5 +1,7 @@
 import React, { useReducer, useEffect, useState } from "react";
 
+import Plot from 'react-plotly.js';
+
 import { ThemeProvider } from "@mui/material/styles";
 import {
     Card,
@@ -14,63 +16,54 @@ import {
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRange } from 'react-date-range';
-import moment from "moment";
 
 import theme from "../theme";
 
-import CanvasJSReact from '../assets/canvasjs.react';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
+const DataVisualizationComponent = (props) => {
 
-const DataVisualizationComponent = () => {
-
-    let yesterday = new Date(Date.now() - 86400000); // 24 * 60 * 60 * 1000
-
-    const [date, setDate] = useState([
-        {
-            startDate: new Date(yesterday),
-            endDate: new Date(),
-            key: 'selection'
-        }
-    ]);
-
-    const options = {
-        theme: "light2", // "light1", "light2", "dark1", "dark2"
-        animationEnabled: true,
-        exportEnabled: true,
-        title: {
-            // text: "Intel Corporation Stock Price -  2017"
-        },
-        axisX: {
-            valueFormatString: "MMM"
-        },
-        axisY: {
-            includeZero: false,
-            prefix: "$",
-            title: "Price (in USD)"
-        },
-        data: [{
-            type: "candlestick",
-            showInLegend: true,
-            name: "Intel Corporation",
-            yValueFormatString: "$###0.00",
-            xValueFormatString: "MMMM YY",
-            dataPoints: [
-
-                // open, high, low, close
-                { x: new Date(date[0].startDate), y: [12.70, 13.05, 12.57, 12.70] },
-                { x: new Date(date[0].endDate), y: [12.95, 13.34, 12.48, 12.84] },
-                // { x: new Date("2017-11-04"), y: [44.73, 47.64, 42.67, 46.16] },
-                // { x: new Date("2017-11-05"), y: [44.73, 47.64, 42.67, 46.16] },
-                // { x: new Date("2017-11-06"), y: [44.73, 47.64, 42.67, 46.16] },
-
-            ]
-        }
-        ]
+    // send snackbar messages to App.js
+    const sendMessageToSnackbar = (msg) => {
+        props.dataFromChild(msg);
     }
 
-    // console.log(`startDate: ${(moment(date[0].startDate).format("l"))}`);
-    // console.log(`endDate: ${(moment(date[0].endDate).format("l"))}`);
+    useEffect(() => {
+        fetchAlphaVantageData('PLTR');
+    }, [])
+
+    const initialState = {
+        stockChartXValues: [],
+        stockChartYValues: []
+    };
+
+    let stockChartXValuesArr = [];
+    let stockChartYValuesArr = [];
+
+    const reducer = (state, newState) => ({ ...state, ...newState });
+    const [state, setState] = useReducer(reducer, initialState);
+
+
+    const fetchAlphaVantageData = async (ticker) => {
+        try {
+            // NOTE: can only make 5 api calls per minute, or 500 api calls per day
+            let alphavantageUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=${ticker}&interval=5min&apikey=${process.env.REACT_APP_API_KEY}`;
+
+            let alphavantageUrlResponse = await fetch(alphavantageUrl);
+            let alphavantageUrlJson = await alphavantageUrlResponse.json();
+
+            for (let date in alphavantageUrlJson['Time Series (Daily)']) {
+                console.log(date)
+                stockChartXValuesArr.push(date);
+                stockChartYValuesArr.push(alphavantageUrlJson['Time Series (Daily)'][date]['1. open']);
+            }
+            setState({ stockChartXValues: stockChartXValuesArr });
+            setState({ stockChartYValues: stockChartYValuesArr });
+            sendMessageToSnackbar(`Found data for ${ticker}`);
+        } catch (error) {
+            sendMessageToSnackbar(`No data for ticker ${ticker}`);
+        }
+    }
+
 
     return (
         <ThemeProvider theme={theme}>
@@ -82,27 +75,31 @@ const DataVisualizationComponent = () => {
                     />
                 </Card>
 
-                <DateRange
+                {/* <DateRange
                     editableDateInputs={true}
                     onChange={item => setDate([item.selection])}
                     moveRangeOnFirstSelection={false}
                     ranges={date}
+                /> */}
+                <Plot
+                    data={[
+                        {
+                            x: state.stockChartXValues,
+                            y: state.stockChartYValues,
+                            type: 'scatter',
+                            //mode: 'lines+markers',
+                            marker: { color: 'green' },
+                        }
+                    ]}
+                    config={{
+                        // turn off modebar on hover
+                        displayModeBar: false                     
+                    }}
+                    layout={{ width: 720, height: 440, title: 'will add title' }}
                 />
-                <Card style={{maxWidth: '50%', marginLeft: '25%'}}>
-                    <div>
-                        {/* <h1>React Candlestick Chart</h1> */}
-                        <CanvasJSChart options={options}
-                        /* onRef={ref => this.chart = ref} */
-                        />
-                        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-                    </div>
-                </Card>
-
-
             </Box>
         </ThemeProvider>
     );
-
 }
 
 export default DataVisualizationComponent;
